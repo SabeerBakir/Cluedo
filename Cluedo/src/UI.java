ab8d0aa307454f26f2a570338e2fcb94a5db3c99
SinfulCitrus <kanexer@gmail.com>
2018-02-03 16:36:45 +0000

    Basic scalling of the UI + Textfield

--- Cluedo/src/UI.java
+++ Cluedo/src/UI.java
@@ -3,39 +3,72 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 
 
 public class UI extends JFrame{
 	
-	private BufferedImage imageBoard;
-	
-	private void getImage(String source) {
+	public class InnerPanel extends JPanel{
 		
-		try {
-			imageBoard = ImageIO.read(new File(source));
-		}
-		catch(IOException exImage) {
-			System.out.print("Image Exception: " + exImage);
-		}
+		// Holds the image to be drawn
+		private BufferedImage imageBuffer;
 		
-	}	
-	
-	private void getMainFrame() {
-		
-		GridLayout mainGL = new GridLayout(0,2);
+		// Constructor that takes in the image to be painted
+		public InnerPanel(String source) {
 			
-		JFrame mainFrame = new JFrame("Main Frame");
-		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainFrame.setVisible(true);
-		mainFrame.setLayout(mainGL);
-		getImage("cluedoboard.jpg");
-		JLabel leftLabel = new JLabel(new ImageIcon(imageBoard));
-		mainFrame.add(leftLabel, mainGL);
-		mainFrame.setSize(1360, 690);
+			super();
+			try {
+				//imageBuffer = ImageIO.read(this.getClass().getResource(source));
+				imageBuffer = ImageIO.read(new File(source));
+			}	
+			catch(IOException exImage1) {
+				System.out.print("Image Exception: " + exImage1.getMessage());
+			}
+			
+		}
+		
+		@Override
+		public void paintComponent(Graphics g) {
+			
+			super.paintComponent(g);
+			Graphics2D g2 = (Graphics2D) g;
+			g2.drawImage(imageBuffer, 0, 0, this.getHeight(), this.getHeight(), this);
+			
+		}
 		
 	}
 	
-	public static void main(String[] Args) {
+	private void getMainFrame() {
+		
+		// Make the main frame
+		JFrame mainFrame = new JFrame("Main Frame");
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setVisible(true);
+		mainFrame.setLayout(new BorderLayout());
+		
+		// Make the appropriate panels
+		InnerPanel boardPanel = new InnerPanel("cluedoboard.jpg");
+		Border loweredetched = BorderFactory.createLineBorder(new Color(0,153,0)); 
+		TitledBorder textBorder = BorderFactory.createTitledBorder(loweredetched, "Commands");
+		textBorder.setTitleJustification(TitledBorder.RIGHT);
+		
+		JTextArea textField = new JTextArea(1,20);
+		textField.setBorder(textBorder);
+		JPanel textPanel = new JPanel();
+		textPanel.setLayout(new BorderLayout());
+		textPanel.add(textField, BorderLayout.CENTER);
+		
+		// Add the panels to main frame
+		mainFrame.add(boardPanel, BorderLayout.CENTER);	
+		mainFrame.add(textPanel, BorderLayout.EAST);
+		mainFrame.setSize(835, 640);
+		
+	}
+	
+	// for testing purposes only
+	public static void main(String[] args) {
 	
 		UI test = new UI();
 		test.getMainFrame();
