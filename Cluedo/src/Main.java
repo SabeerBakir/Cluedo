a7640fabf0e69e39abfd8cbd21b7cc97688d6233
SabeerBakir <sabeer.bakir@ucdconnect.ie>
2018-02-08 20:21:56 +0000

    Player initialisation

--- Cluedo/src/Main.java
+++ Cluedo/src/Main.java
@@ -2,11 +2,7 @@ import java.util.Random;
 
 public class Main {
 	
-	public String[] suspects = {"Plum", "White", "Scarlet", "Green", "Mustard", "Peacock"};
-	public String[] weapons = {"Rope", "Dagger", "Wrench", "Pistol", "Candlestick", "Lead Pipe"};
-	public String[] rooms = {"Courtyard", "Game Room", "Study", "Dining Room", "Garage", "Living Room", "Kitchen", "Bedroom", "Bathroom"};
-	
-	public int[][] spawnLocations = new int[][] {
+	static public int[][] spawnLocations = new int[][] {
 		{0, 9},
 		{0, 14},
 		{6, 23},
@@ -15,13 +11,30 @@ public class Main {
 		{24,7}
 	};
 	
-	// Killer cards
-	Random rand = new Random();
-	String killerName = suspects[rand.nextInt(suspects.length)];
-	String killerWeapon = weapons[rand.nextInt(weapons.length)];
-	String killerRoom = rooms[rand.nextInt(rooms.length)];
-	
 	public static void main(String[] args) {
+		
+		String[] suspects = {"Plum", "White", "Scarlet", "Green", "Mustard", "Peacock"};
+		String[] weapons = {"Rope", "Dagger", "Wrench", "Pistol", "Candlestick", "Lead Pipe"};
+		String[] rooms = {"Courtyard", "Game Room", "Study", "Dining Room", "Garage", "Living Room", "Kitchen", "Bedroom", "Bathroom"};
+		
+		// Killer cards
+		Random rand = new Random();
+		String killerName = suspects[rand.nextInt(suspects.length)];
+		String killerWeapon = weapons[rand.nextInt(weapons.length)];
+		String killerRoom = rooms[rand.nextInt(rooms.length)];
+		
+		Player[] players = new Player[6];
+		createPlayers(players);
 		System.out.println("Boosted Programmers");
+		
+	}
+	
+	static void createPlayers(Player players[]){
+		Random rand = new Random();
+		
+		for(int i = 0; i < players.length; i++){
+			players[i] = new Player(i, spawnLocations[i][0], spawnLocations[i][1]);
+			System.out.println(players[i].getPosX() + " " + players[i].getPosY());
+		}
 	}
 }
