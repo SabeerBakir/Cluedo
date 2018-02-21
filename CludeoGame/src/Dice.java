84ab67e2668a80515cd044586c04c808ce686e08
SinfulCitrus <kanexer@gmail.com>
2018-02-21 20:59:54 +0000

    Dice Class, UI Changes, Ignore update
    A Dice class was added
    UI format was adjusted
    /bin directory will no longer be included in the repository

---
+++ CludeoGame/src/Dice.java
@@ -0,0 +1,46 @@
+import java.util.Random;
+
+public class Dice {
+	
+	private Random randFace = new Random();
+	private int numberDice;
+	private int sides;
+
+	public Dice() {
+		this.numberDice = 1;
+		this.sides = 6;
+	}
+	
+	public Dice(int sides) {
+		this.sides = sides;
+	}
+	
+	public Dice(int sides, int numberDice) {
+		this.numberDice = numberDice;
+		this.sides = sides;
+	}
+	
+	private int generateFace() {
+		return (randFace.nextInt(sides)+1);
+	}
+	
+	public int getNumber() {
+		return this.numberDice;
+	}
+	
+	public int getSides() {
+		return this.sides;
+	}
+	
+	public int roll() {
+		int total = 0;
+		for(int i = 0; i < numberDice; i++) total+=generateFace();
+		return total;
+	}
+	
+	@Override
+	public String toString() {
+		return "{ Sides: " + sides + ", Quantity: " + numberDice + " }";
+	}
+	
+}
--- CludeoGame/src/UI.java
+++ CludeoGame/src/UI.java
@@ -1,31 +1,45 @@
 import java.awt.BorderLayout;
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
 
 public class UI {
 
-    private static final int FRAME_WIDTH = 1200;
-    private static final int FRAME_HEIGHT = 800;
+    private static final int FRAME_WIDTH = 1205;
+    private static final int FRAME_HEIGHT = 790;
 
     private final BoardPanel boardPanel;
     private final InfoPanel infoPanel = new InfoPanel();
     private final CommandPanel commandPanel = new CommandPanel();
-
+    
+    // Make the appropriate panels
+ 	Border loweredetched = BorderFactory.createLineBorder(commandPanel.getBackground()); 
+ 	TitledBorder textInfoBorder = BorderFactory.createTitledBorder(loweredetched, "Info"); 
+ 	TitledBorder textComdBorder = BorderFactory.createTitledBorder(loweredetched, "Commands"); 	
+ 	
     UI(Tokens characters, Weapons weapons) {
         JFrame frame = new JFrame();
         boardPanel = new BoardPanel(characters, weapons);
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
-        frame.setTitle("Cluedo");
+        frame.setTitle("FinnaBustaJava");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.add(boardPanel, BorderLayout.LINE_START);
-        frame.add(infoPanel, BorderLayout.LINE_END);
-        frame.add(commandPanel, BorderLayout.PAGE_END);
+     	textInfoBorder.setTitleJustification(TitledBorder.CENTER);
+     	textComdBorder.setTitleJustification(TitledBorder.CENTER);
+        infoPanel.setBorder(textInfoBorder);
+        commandPanel.setBorder(textComdBorder);
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new BorderLayout());
+        leftPanel.add(infoPanel, BorderLayout.CENTER);
+        leftPanel.add(commandPanel, BorderLayout.SOUTH);
+        frame.add(leftPanel, BorderLayout.EAST);
         frame.setResizable(false);
         frame.setVisible(true);
     }
 
     public String getCommand() {
         return commandPanel.getCommand();
-    }
+    }	
 
     public void display() {
         boardPanel.refresh();
